2083a78c4e12e60f767d70dd6a28b5649f6506d3
swampC <wolfwolf28wolf@gmail.com>
2025-08-05 22:58:41 -0400

    reverted back to the old compression since splitting+compressing+merging is actually slower (wow who would've guessed) always prefer simplicity i guess

--- src/backend/services/PDFCompressService.ts
+++ src/backend/services/PDFCompressService.ts
@@ -1,152 +1,105 @@
 import { createSession, cleanSession, PDFGzip } from '../utils/utils.ts';
-import { readdir } from 'node:fs/promises';
 import type { SessionID } from '../utils/utils.ts';
-
-async function compressDefault(files: string[], id: SessionID) {
-    try {
-        for (const page of files) {
-            const output = `compressed-${page}`;
-            await Bun.spawn(
-                [
-                    'gs',
-                    '-sDEVICE=pdfwrite',
-                    '-dCompatibilityLevel=1.4',
-                    '-dPDFSETTINGS=/ebook',
-                    '-dNOPAUSE',
-                    '-dQUIET',
-                    '-dBATCH',
-                    `-sOutputFile=${output}`,
-                    '-c',
-                    '30000000',
-                    'setvmthreshold',
-                    '-f',
-                    page,
-                ],
-                {
-                    cwd: `./src/backend/sessions/${id}`,
-                }
-            ).exited;
-        }
-        const dirFiles = await readdir(`./src/backend/sessions/${id}`);
-        const compressedFiles: string[] = dirFiles.filter((file) => file.startsWith('compressed-'));
-        return compressedFiles;
-    } catch (err) {
-        throw err;
-    }
-}
-
-async function splitFile(id: SessionID): Promise<string[]> {
-    try {
-        await Bun.spawn(
-            [
-                'gs',
-                '-dBATCH',
-                '-dNOPAUSE',
-                '-sDEVICE=pdfwrite',
-                '-dSAFER',
-                '-sOutputFile=page-%03d.pdf',
-                'input.pdf',
-            ],
-            { cwd: `./src/backend/sessions/${id}` }
-        ).exited;
-        const files = await readdir(`./src/backend/sessions/${id}`);
-        const splitFiles: string[] = files.filter((file) => file.startsWith('page'));
-
-        return splitFiles;
-    } catch (err) {
-        throw err;
-    }
-}
-
-async function compressAggressive(files: string[], id: SessionID) {
-    try {
-        for (const page of files) {
-            const output = `compressed-${page}`;
-            await Bun.spawn(
-                [
-                    'gs',
-                    '-sDEVICE=pdfwrite',
-                    '-dCompatibilityLevel=1.4',
-                    '-dPDFSETTINGS=/screen',
-                    '-dColorConversionStrategy=/Gray',
-                    '-dProcessColorModel=/DeviceGray',
-                    '-dColorImageResolution=72',
-                    '-dGrayImageResolution=72',
-                    '-dMonoImageResolution=72',
-                    '-dDownsampleColorImages=true',
-                    '-dDownsampleGrayImages=true',
-                    '-dDownsampleMonoImages=true',
-                    '-dCompressFonts=true',
-                    '-dSubsetFonts=true',
-                    '-dNOPAUSE',
-                    '-dQUIET',
-                    '-dBATCH',
-                    `-sOutputFile=${output}`,
-                    '-c',
-                    '30000000',
-                    'setvmthreshold',
-                    '-f',
-                    page,
-                ],
-                {
-                    cwd: `./src/backend/sessions/${id}`,
-                }
-            ).exited;
-        }
-        const dirFiles = await readdir(`./src/backend/sessions/${id}`);
-        const compressedFiles: string[] = dirFiles.filter((file) => file.startsWith('compressed-'));
-        return compressedFiles;
-    } catch (err) {
-        throw err;
-    }
-}
-
-async function mergeCompressedFiles(compressedFiles: string[], id: SessionID) {
-    try {
-        await Bun.spawn(
-            [
-                'gs',
-                '-dBATCH',
-                '-dNOPAUSE',
-                '-q',
-                '-sDEVICE=pdfwrite',
-                '-sOutputFile=output.pdf',
-                ...compressedFiles,
-            ],
-            {
-                cwd: `./src/backend/sessions/${id}`,
-            }
-        ).exited;
-    } catch (err) {
-        throw err;
-    }
-}
 
 export async function PDFCompressService(aggressive: Boolean, file: Buffer) {
     try {
         const id: SessionID = await createSession();
-        await Bun.write(`./src/backend/sessions/${id}/input.pdf`, file);
-        const splitFiles: string[] = await splitFile(id);
 
         if (aggressive) {
-            const aggressiveCompress: string[] = await compressAggressive(splitFiles, id);
+            const aggressiveCompress: Buffer = await compressAggressive(file, id);
 
-            await mergeCompressedFiles(aggressiveCompress, id);
-
-            const outputFile = Bun.file(`./src/backend/sessions/${id}/output.pdf`);
-            const outputBuffer: Buffer = Buffer.from(await outputFile.arrayBuffer());
-            const outputGZIP: Buffer = await PDFGzip(outputBuffer);
             cleanSession(id);
-            return outputGZIP;
+            return aggressiveCompress;
         }
-        const defaultCompress: string[] = await compressDefault(splitFiles, id);
-        await mergeCompressedFiles(defaultCompress, id);
+        const defaultCompress: Buffer = await compressDefault(file, id);
 
+        cleanSession(id);
+        return defaultCompress;
+    } catch (err) {
+        throw err;
+    }
+}
+
+async function compressDefault(file: Buffer, id: SessionID) {
+    try {
+        await Bun.write(`./src/backend/sessions/${id}/input.pdf`, file);
+        //NOTE: After this handle the actual compressing using pdfcpu
+        const proc = Bun.spawn(
+            [
+                'gs',
+                '-sDEVICE=pdfwrite',
+                '-dCompatibilityLevel=1.4',
+                '-dPDFSETTINGS=/ebook',
+                '-dNOPAUSE',
+                '-dQUIET',
+                '-dBATCH',
+                '-dNOGC',
+                '-dNumRenderingThreads=4',
+                '-sOutputFile=output.pdf',
+                '-c',
+                '30000000',
+                'setvmthreshold',
+                '-f',
+                'input.pdf',
+            ],
+            {
+                cwd: `./src/backend/sessions/${id}`,
+            }
+        );
+        await proc.exited;
         const outputFile = Bun.file(`./src/backend/sessions/${id}/output.pdf`);
         const outputBuffer: Buffer = Buffer.from(await outputFile.arrayBuffer());
-        const outputGZIP: Buffer = await PDFGzip(outputBuffer);
-        cleanSession(id);
-        return outputGZIP;
+        const gzippedResponse: Buffer = await PDFGzip(outputBuffer);
+        //INFO: Clean in the background, responding to the user is priority
+        return gzippedResponse;
+    } catch (err) {
+        throw err;
+    }
+}
+
+async function compressAggressive(file: Buffer, id: SessionID) {
+    try {
+        await Bun.write(`./src/backend/sessions/${id}/input.pdf`, file);
+        //NOTE: After this handle the actual compressing using pdfcpu
+        const proc = Bun.spawn(
+            [
+                'gs',
+                '-sDEVICE=pdfwrite',
+                '-dCompatibilityLevel=1.4',
+                '-dColorImageDownsampleType=/Bicubic',
+                '-dColorConversionStrategy=/Gray',
+                '-dProcessColorModel=/DeviceGray',
+                '-dColorImageResolution=72',
+                '-dGrayImageResolution=72',
+                '-dMonoImageResolution=72',
+                '-dDownsampleColorImages=true',
+                '-dDownsampleGrayImages=true',
+                '-dDownsampleMonoImages=true',
+                '-dCompressFonts=true',
+                '-dSubsetFonts=true',
+                '-dNOGC',
+                '-dNumRenderingThreads=4',
+                '-dNOPAUSE',
+                '-dQUIET',
+                '-dBATCH',
+                '-sOutputFile=output.pdf',
+                '-c',
+                '30000000',
+                'setvmthreshold',
+                '-f',
+                'input.pdf',
+            ],
+            {
+                cwd: `./src/backend/sessions/${id}`,
+            }
+        );
+        await proc.exited;
+        const outputFile = Bun.file(`./src/backend/sessions/${id}/output.pdf`);
+        const outputBuffer: Buffer = Buffer.from(await outputFile.arrayBuffer());
+        console.time('GZIP');
+        const gzippedResponse: Buffer = await PDFGzip(outputBuffer);
+        console.timeEnd('GZIP');
+        return gzippedResponse;
     } catch (err) {
         throw err;
     }
