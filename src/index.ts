4f89aaff6b69107181b862fc042060192bd5c233
swampC <wolfwolf28wolf@gmail.com>
2025-08-14 14:21:09 -0400

    Website endpoints added to server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono';
+import { serveStatic } from 'hono/bun';
 import { logger } from 'hono/logger';
 import MainRouter from './backend/routers/MainRouter.ts';
 
@@ -6,9 +7,21 @@ const app = new Hono();
 
 app.use(logger());
 
-app.get('/', (c) => {
-    return c.text('Hello Hono!');
-});
+//INFO: WEBSITE ASSETS AND PAGES
+app.use('/icons/*', serveStatic({ root: './src/public/' }));
+app.use('/pdf/compress/*', serveStatic({ root: './src/public/' }));
+app.use('/utils/*', serveStatic({ root: './src/public/' }));
+app.use('/pdf/convert/*', serveStatic({ root: './src/public/' }));
+app.use('/png/compress/*', serveStatic({ root: './src/public/' }));
+app.use('/png/convert/*', serveStatic({ root: './src/public/' }));
+app.use('/jpeg/convert/*', serveStatic({ root: './src/public/' }));
+app.use('/jpeg/compress/*', serveStatic({ root: './src/public/' }));
+app.use('/webp/compress/*', serveStatic({ root: './src/public/' }));
+app.use('/webp/convert/*', serveStatic({ root: './src/public/' }));
+app.use('/mp4/convert/*', serveStatic({ root: './src/public/' }));
+app.use('/mp4/compress/*', serveStatic({ root: './src/public/' }));
+app.use('/shared/*', serveStatic({ root: './src/public' }));
+app.use('/*', serveStatic({ root: './src/public/home/' }));
 
 //INFO: API ROUTES
 app.route('/api', MainRouter);
